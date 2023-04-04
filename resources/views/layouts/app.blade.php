536428686827ebba1f3f81476f7e1b96cd9735f7
renzcabato1 <cabato.renz.renz@gmail.com>
2023-04-04 11:17:33 +0800

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta charset="UTF-8">
+	<meta name="description" content="Swap, earn, and build on the leading decentralized crypto trading protocol">
+	<meta name="keywords" content="cryptocurrency, hardfork, Collect Whiskey, Live Auctions">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/icon.png" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="{{asset('home/css/bootstrap.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('home/css/font-awesome.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('home/css/themify-icons.css')}}"/>
+	<link rel="stylesheet" href="{{asset('home/css/animate.css')}}"/>
+	<link rel="stylesheet" href="{{asset('home/css/owl.carousel.css')}}"/>
+	<link rel="stylesheet" href="{{asset('home/css/style.css')}}"/>
+
+</head>
+<body class='bg-dark'>
+        <div id="preloder">
+            <div class="loader"></div>
+        </div>
+        @yield('header')
+        @yield('content')
+
+        <script src="{{asset('home/js/jquery-3.2.1.min.js')}}"></script>
+        <script src="{{asset('home/js/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('home/js/main.js')}}"></script>
+</body>
+</html>
